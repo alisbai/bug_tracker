85e8f8d40e8a7aac47b5702bc90bbf6d2838e33d
alisbai <ali.sbai.96@gmail.com>
2023-01-15 18:47:01 +0100

    added fk ticket_id to tickets table

---
+++ database/migrations/2023_01_15_173743_add_ticket_id_column_to_comments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->unsignedBigInteger("ticket_id")->after("id");
+            $table->foreign("ticket_id")->references("id")->on("tickets")->cascadeOnDelete()->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->dropColumn("ticket_id");
+            $table->dropForeign("ticket_id");
+        });
+    }
+};
