41033a8ce6975d81e55b78b55fc6b9e94d91c808
alisbai <ali.sbai.96@gmail.com>
2023-01-17 06:32:25 +0100

    created a signup form

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -5,6 +5,26 @@ import { useState } from 'react';
 import { router as inertiaRouter } from '@inertiajs/react';
 
 export default function Welcome(props) {
+
+    const [isLoginVisible, setIsLoginVisible] = useState(true);
+
+    function handleClick() {
+        alert("click")
+        setIsLoginVisible(!isLoginVisible);
+    }
+    
+    return (
+        <>
+            <Head title="Bug Tracker" />
+            <div className='flex justify-center items-center w-screen h-screen background-image-container'>
+                {isLoginVisible ?<LoginForm onClick={handleClick}/> : <SignupForm  onClick={handleClick}/>}
+            </div>
+
+        </>
+    );
+}
+
+function LoginForm({onClick}) {
     const [values, setValues] = useState({
         email: "",
         password:""
@@ -22,11 +42,9 @@ export default function Welcome(props) {
         e.preventDefault();
         inertiaRouter.get(route("dashboard"));
     }
+    
     return (
-        <>
-            <Head title="Bug Tracker" />
-            <div className='flex justify-center items-center w-screen h-screen background-image-container'>
-                <form onSubmit={handleSubmit} className='w-4/5 md:w-1/3 lg:w-1/4 flex flex-col p-5 bg-slate-50 rounded'>
+        <form onSubmit={handleSubmit} className='w-4/5 md:w-1/3 lg:w-1/4 flex flex-col p-5 bg-slate-50 rounded'>
                     <div className='flex flex-col mb-4'>
                         <label htmlFor='email' className='text-sm'>Email</label>
                         <TextInput id="email" className="w-full" type="email" required="required" handleChange={handleChange}/>
@@ -36,10 +54,51 @@ export default function Welcome(props) {
                         <TextInput id= "password" className="w-full" type="password" required="required" handleChange={handleChange} />
                     </div>
                     <PrimaryButton className='flex justify-center mb-1' children="Login"/>
-                    <small className='text-slate-500'><Link href='#'>Signup</Link></small>
+                    <small className='text-slate-500 cursor-pointer' onClick={onClick}>Signup</small>
                 </form>
-            </div>
+    )
+}
 
-        </>
-    );
+
+function SignupForm({onClick}) {
+    const [values, setValues] = useState({
+        email: "",
+        password:""
+    })
+    function handleChange(e) {
+        const key = e.target.id;
+        const value = e.target.value;
+        setValues({
+            ...values,
+            [key]: value
+        })
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        inertiaRouter.get(route("dashboard"));
+    }
+    
+    return (
+        <form onSubmit={handleSubmit} className='w-4/5 md:w-1/3 lg:w-1/4 flex flex-col p-5 bg-slate-50 rounded'>
+                    <div className='flex flex-col mb-4'>
+                        <label htmlFor='first-name' className='text-sm'>First Name</label>
+                        <TextInput id="first-name" className="w-full" type="text" required="required" handleChange={handleChange}/>
+                    </div>
+                    <div className='flex flex-col mb-4'>
+                        <label htmlFor='last-name' className='text-sm'>Last Name</label>
+                        <TextInput id="last-name" className="w-full" type="text" required="required" handleChange={handleChange}/>
+                    </div>
+                    <div className='flex flex-col mb-4'>
+                        <label htmlFor='email' className='text-sm'>Email</label>
+                        <TextInput id="email" className="w-full" type="email" required="required" handleChange={handleChange}/>
+                    </div>
+                    <div className='flex flex-col mb-4'>
+                        <label htmlFor='password' className='text-sm'>Password</label>
+                        <TextInput id= "password" className="w-full" type="password" required="required" handleChange={handleChange} />
+                    </div>
+                    <PrimaryButton className='flex justify-center mb-1' children="Login"/>
+                    <small className='text-slate-500 cursor-pointer' onClick={onClick}>login</small>
+                </form>
+    )
 }
